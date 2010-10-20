b77835445eb4cef1afd36a53b38fdcc112a545e3
Micah Frost <micah.frost@fracturedatlas.org>
2010-10-20 17:21:35 -0400

    Updated field seeds, ticket generator

--- tix_tickets.rb
+++ tix_tickets.rb
@@ -9,26 +9,16 @@ require 'json'
 require 'yaml'
 require 'athena'
 
-(1..2).each do |i|
-  ticket_hash = Hash.new
-  ticket_hash['PERFORMANCE'] = '2010-11-0' + i.to_s + 'T09:00:00-04:00'
-  ticket_hash['SOLD'] = 'false'
-  ticket_hash['VENUE'] = 'St. James Theater'
-  ticket_hash['TITLE'] = 'Oklahoma'
+ticket = {}
+ticket['PERFORMANCE'] = "2010-10-20T17:17:30-04:00"
+ticket['SOLD'] = 'false'
+ticket['VENUE'] = 'St. James Theater'
+ticket['TITLE'] = 'Oklahoma'
 
-  ['A', 'B'].each do |sec|
-    ticket_hash['SECTION'] = sec
-    if sec.eql? 'A'
-      ticket_hash['TIER'] = 'GOLD'
-      ticket_hash['PRICE'] = '100'
-    else
-      ticket_hash['TIER'] = 'NONE'
-      ticket_hash['PRICE'] = '50'
-    end
 
-    (0..2).each do |seat_num|
-      ticket_hash['SEAT_NUMBER'] = seat_num.to_s
-      ap Athena.create_ticket(ticket_hash)
-    end
+[50,100].each do |price|
+  ticket['PRICE'] = price
+  10.times do
+    ap Athena.create_ticket(ticket)
   end
 end
