37c1e2a609bb2929068e4ec529bd50219eec27b7
Gary Moore <gary.moore@fracturedatlas.org>
2010-11-29 17:59:31 -0500

    Added script to deploy all relevant wars.  Run from athena root directory

---
+++ deploy.rb
@@ -0,0 +1,4 @@
+system ("asadmin deploy --force=true components/tix/target/tix.war")
+system ("asadmin deploy --force=true components/stage/target/stage.war")
+system ("asadmin deploy --force=true components/people/target/people.war")
+system ("asadmin deploy --force=true components/payments/target/payments.war")
